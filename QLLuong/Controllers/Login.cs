50a6433e95648bc913a88f124a5c7fef21dd0864
Bien1109 <bien11092k4@gmail.com>
2024-10-14 14:30:44 +0700

    cauhinhHome

---
+++ QLLuong/Controllers/Login.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLLuong.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
