12a7ee5a32c0dc95e04bccb61fe9000505ee46dc
vuong duc hung <vuongduchung2004@gmail.com>
2024-10-15 09:43:38 +0700

    create KTKL

---
+++ QLLuong/Controllers/KTKLController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLLuong.Controllers
+{
+	public class KTKLController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
