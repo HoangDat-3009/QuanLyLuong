f06295b8ae6520a8ac109edf2ba3a4ff8cfe2d4c
vuong duc hung <vuongduchung2004@gmail.com>
2024-10-16 09:53:11 +0700

    add data

---
+++ QLLuong/Data/QLLuongContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QLLuong.Data
+{
+    public class QLLuongContext : DbContext
+    {
+        public QLLuongContext(DbContextOptions<QLLuongContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<QLLuong.Models.NhanVien> NhanViens { get; set; }
+        public DbSet<QLLuong.Models.Luong> Luongs { get; set; }
+        public DbSet<QLLuong.Models.KTKL> KTKLs { get; set; }
+    }
+}
--- QLLuong/Models/NhanVien.cs
+++ QLLuong/Models/NhanVien.cs
@@ -1,20 +1,35 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace QLLuong.Models
 {
     public class NhanVien
     {
+        [Key]
         public int MaNhanVien { get; set; }
+        [Required]
         public string? HoTen { get; set; }
+        [Required]
         public string? GioiTinh { get; set; }
+        [Required]
         public DateTime NgaySinh { get; set; }
+        [Required]
         public string? NoiSinh { get; set; }
+        [Required]
         public int? MaDanToc { get; set; }
+        [Required]
         public int? MaChucVu { get; set; }
+        [Required]
         public int? MaPhongBan { get; set; }
+        [Required]
         public int? MaTrinhDo { get; set; }
+        [Required]
         public int? MaChuyenMon { get; set; }
+        [Required]
         public string? DiaChi { get; set; }
+        [Required]
         public string? DienThoai { get; set; }
+        [Required]
         public int? MaHeSo { get; set; }
     }
 }
