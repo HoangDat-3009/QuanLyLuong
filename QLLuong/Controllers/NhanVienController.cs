49344e69b9c3000a225dccb97be223c10e8422f3
vuong duc hung <vuongduchung2004@gmail.com>
2024-10-16 09:08:06 +0700

    fix model

--- QLLuong/Controllers/NhanVienController.cs
+++ QLLuong/Controllers/NhanVienController.cs
@@ -6,28 +6,23 @@ namespace QLLuong.Controllers
     public class NhanVienController : Controller
     {
         private List<NhanVien> listNhanVien = new List<NhanVien>();
-        private NhanVien inforStaff = new NhanVien();
         public NhanVienController()
         {
             listNhanVien = new List<NhanVien>()
-            {
-                new NhanVien()
                 {
-                    Id = 1,Name = "Tran Van A",Email = " abc@gmail.com",Address="so 1 abc",DateOfBirth=new DateTime()
-                },
-                new NhanVien()
-                {
-                    Id = 2,Name = "Tran Van B",Email = " abcd@gmail.com",Address="so 2 abc",DateOfBirth=new DateTime()
-                }
-            };
+                    new NhanVien()
+                    {
+                        MaNhanVien = 1, HoTen = "Tran Van A", DiaChi = "so 1 abc", NgaySinh = DateTime.Now
+                    },
+                    new NhanVien()
+                    {
+                        MaNhanVien = 2, HoTen = "Tran Van B", DiaChi = "so 2 abc", NgaySinh = DateTime.Now
+                    }
+                };
         }
         public IActionResult Index()
         {
             return View(listNhanVien);
-        }
-        public IActionResult Index_Infor_Staff()
-        {
-            return View(inforStaff);
         }
     }
 }
--- QLLuong/Models/NhanVien.cs
+++ QLLuong/Models/NhanVien.cs
@@ -1,47 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Reflection;
-
+﻿
 namespace QLLuong.Models
 {
     public class NhanVien
     {
-        public int Id { get; set; }//Mã sinh viên
-        [StringLength(100, MinimumLength = 4, ErrorMessage = "Tên phải có ít nhất 4 ký tự")]
-        [Required(ErrorMessage = "Phải nhập Họ và tên")]
-        [Display(Name = "Họ tên")]
-
-        public string? Name { get; set; } //Họ tên
-        [RegularExpression(@"^[^@\s]+@gmail\.com$", ErrorMessage = "Email phải có định dạng @gmail.com")]
-        [Required(ErrorMessage = "Phải nhập Email")]
-        [Display(Name = "Email")]
-        public string? Email { get; set; } //Email
-        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-    ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên, có ít nhất một ký tự viết hoa, một ký tự viết thường, một chữ số và một ký tự đặc biệt.")]
-        [Required(ErrorMessage = "Không được bỏ trống")]
-        /*public string? Password { get; set; }//Mật khẩu
-        [Required(ErrorMessage = "Không được bỏ trông mục này")]
-        [DataType(DataType.Password)]*/
-
-/*        [Display(Name = "Ngành")]
-        public Branch? Branch { get; set; }//Ngành học
-        [Required(ErrorMessage = "Không được bỏ trông mục này")]*/
-        [Display(Name = "Giới tính")]
-        public Gender? Gender { get; set; }//Giới tính
-
-       /* [Display(Name = "Hệ")]
-
-        public bool IsRegular { get; set; }//Hệ: true-chính quy, false-phi chính quy
-        [DataType(DataType.MultilineText)]
-        [Required(ErrorMessage = "Không được bỏ trông mục này")]*/
-
-        [Display(Name = "Địa chỉ")]
-        public string? Address { get; set; }//Địa chỉ
-        [Range(typeof(DateTime), "1/1/1963", "31/12/2020")]
-        [DataType(DataType.Date)]
-        [Required]
-        public DateTime DateOfBirth { get; set; }//Ngày sinh
-       /* [Range(0.0, 10.0)]
-        [Required]
-        public float? Point { get; set; }//Ngày sinh*/
+        public int MaNhanVien { get; set; }
+        public string? HoTen { get; set; }
+        public string? GioiTinh { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string? NoiSinh { get; set; }
+        public int? MaDanToc { get; set; }
+        public int? MaChucVu { get; set; }
+        public int? MaPhongBan { get; set; }
+        public int? MaTrinhDo { get; set; }
+        public int? MaChuyenMon { get; set; }
+        public string? DiaChi { get; set; }
+        public string? DienThoai { get; set; }
+        public int? MaHeSo { get; set; }
     }
 }
