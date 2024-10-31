d3bd5911f4bf0bff485330603210698c433909c2
vuong duc hung <vuongduchung2004@gmail.com>
2024-10-31 23:55:44 +0700

    uppppppppp

--- QLLuong/Models/NhanVien.cs
+++ QLLuong/Models/NhanVien.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace QLLuong.Models;
 
 public partial class NhanVien
 {
-    [Key]
     public int MaNhanVien { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
+    public bool IsDeleted { get; set; }
+
     public string? HoTen { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? GioiTinh { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public DateOnly? NgaySinh { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? NoiSinh { get; set; }
 
     public DateOnly? NgayVaoCongTy { get; set; }
@@ -28,19 +28,24 @@ public partial class NhanVien
     public int? MaTrinhDo { get; set; }
 
     public int? MaChuyenMon { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? DiaChi { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? DienThoai { get; set; }
-    public bool IsDeleted { get; set; }
 
     public int? MaHeSo { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? Cccd { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? TaiKhoanNganHang { get; set; }
-    [Required(ErrorMessage = "Không được bỏ trống mục này")]
+
     public string? SoTaiKhoanNganHang { get; set; }
+
+    public virtual ICollection<ChamCong> ChamCongs { get; set; } = new List<ChamCong>();
+
+    public virtual ICollection<KhenThuongKyLuat> KhenThuongKyLuats { get; set; } = new List<KhenThuongKyLuat>();
+
+    public virtual ICollection<LuongCu> LuongCus { get; set; } = new List<LuongCu>();
 
     public virtual ICollection<Luong> Luongs { get; set; } = new List<Luong>();
 
@@ -55,4 +60,6 @@ public partial class NhanVien
     public virtual PhongBan? MaPhongBanNavigation { get; set; }
 
     public virtual TrinhDo? MaTrinhDoNavigation { get; set; }
+
+    public virtual UserLogin? UserLogin { get; set; }
 }
